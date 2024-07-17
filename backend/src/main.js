b601929bf1fcdb3bb0ad829913f00407fca81e83
JulliaFernandes <julliafernandes41@hotmail.com>
2024-07-17 19:42:35 -0300

    created the controller and route to resend the email

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,8 +3,10 @@ const api = express();
 const env = require('./env')
 
 const sendMailController  = require('./controllers/sendMail')
+const resendMailController = require('./controllers/resendMail')
 
 api.use(express.json())
 api.post('/send-mail', sendMailController)
+api.post('/resend-mail', resendMailController)
 
 api.listen(env.PORT, () => console.log(`Back-End running at ${env.PORT}`))
