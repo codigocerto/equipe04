35772f245bdb494c7318f08002d038d1ee0b3cdd
JulliaFernandes <julliafernandes41@hotmail.com>
2024-07-18 06:51:36 -0300

    Deleted the controller to cancel newsletter

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,11 +4,9 @@ const env = require('./env')
 
 const sendMailController  = require('./controllers/sendMail')
 const resendMailController = require('./controllers/resendMail')
-const cancelNewsletterController = require('./controllers/cancelNewsletter')
 
 api.use(express.json())
 api.post('/send-mail', sendMailController)
 api.post('/resend-mail', resendMailController)
-api.post('/cancel-newsletter/:userId', cancelNewsletterController);
 
 api.listen(env.PORT, () => console.log(`Back-End running at ${env.PORT}`))
