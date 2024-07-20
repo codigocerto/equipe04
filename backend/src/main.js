f678bd2b687e3d5146dec65547cbd0455608902a
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-07-20 14:27:27 -0300

    refactor: Criação da rota para o re-envio do email de cadastro

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,11 +3,13 @@ const api = express();
 const env = require("./env");
 
 const sendMailController = require("./controllers/sendMailController");
+const resendMailController = require("./controllers/resendMailController");
 const updateNewsletterController = require("./controllers/updateNewsletterController");
 
 api.use(express.json());
 
 api.post("/cadastro", sendMailController);
+api.post("/resend-mail", resendMailController);
 api.patch("/update-newsletter", updateNewsletterController);
 
 api.listen(env.PORT, () => console.log(`Back-End running at ${env.PORT}`));
