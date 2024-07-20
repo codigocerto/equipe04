4f61a6d2cdd2e83a3c8691ac19a8dcb0690f21d0
Emerson Bezerra <emersonbbezerra@gmail.com>
2024-07-20 15:06:51 -0300

    refactor: Alterado o modo de captura do email na rota de reenvio para o "Request Query" no lugar do "Request Body".

--- backend/src/controllers/resendMailController.js
+++ backend/src/controllers/resendMailController.js
@@ -1,35 +1,29 @@
-const { z } = require("zod");
 const prisma = require("../configs/prisma");
 const mailProvider = require("../mailProvider/mailProvider");
 
 const resendMailController = async (request, response) => {
-    if (!request.body) {
-        return response.status(400).json({ error: 'Email não informado' });
-    }
-    
-    const emailSchema = z.object({
-        email: z.string().email()
+  const { email } = request.query;
+
+  if (!email) {
+    return response.status(400).json({ error: "Email não informado" });
+  }
+
+  try {
+    const user = await prisma.users.findUnique({
+      where: { email },
     });
-    
-    try{
-        const { email } = emailSchema.parse(request.body);
 
-        const user = await prisma.users.findUnique({
-            where: { email: email }
-        });
-
-        if (!user) {
-            return response.status(400).json({ error: 'Email não encontrado' });
-        }
-        
-        mailProvider(email, "Reenvio de Email", `<h1>Reenvio de Email</h1>`);
-
-        response.status(200).json({ message: 'Email reenviado com sucesso' });
+    if (!user) {
+      return response.status(404).json({ error: "Email não encontrado" });
     }
-    catch (error) {
-        console.log(error);
-        return response.status(500).json({ error: 'Erro ao reenviar email' });
-    }
+
+    mailProvider(user.email, "Reenvio de Email", `<h1>Reenvio de Email</h1>`);
+
+    response.status(200).json({ message: "Email reenviado com sucesso" });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: "Erro ao reenviar email" });
+  }
 };
 
 module.exports = resendMailController;
