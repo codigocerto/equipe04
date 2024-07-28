463562113a22e389bb3f7e52261ecc8a46dfb576
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-07-28 15:29:41 -0300

    ci: inclusao do workflow de pr

--- cypress/e2e/frontend/accordion.cy.js
+++ cypress/e2e/frontend/accordion.cy.js
@@ -1,6 +1,6 @@
 describe('Componente Accordion', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/frontend/index.html')
+    cy.visit('http://127.0.0.1:5500/frontend')
   })
 
   it('Verifica se o accordion foi renderizado', () => {
